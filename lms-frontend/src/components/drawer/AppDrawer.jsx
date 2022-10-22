715f73c201f44f19edae2a11a0b5cbb4bb2c64e8
julinbestina <julin.bestina@yahoo.com>
2022-10-22 11:10:00 +0530

    Modified:Main layout css

--- lms-frontend/src/components/drawer/AppDrawer.jsx
+++ lms-frontend/src/components/drawer/AppDrawer.jsx
@@ -1,80 +1,206 @@
-import * as React from 'react';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import DeveloperBoardIcon from '@mui/icons-material/DeveloperBoard';
-import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import { React, useState } from "react";
+import {
+  Drawer,
+  CssBaseline,
+  AppBar,
+  Box,
+  Tooltip,
+  Avatar,
+  createTheme,
+  ThemeProvider,
+  styled,
+  Divider,Typography
+} from "@mui/material";
+import PropTypes from 'prop-types';
+import TreeView from '@mui/lab/TreeView';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import TreeItem from '@mui/lab/TreeItem';
+import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
+import ZoomInOutlinedIcon from '@mui/icons-material/ZoomInOutlined';
 import AssessmentIcon from '@mui/icons-material/Assessment';
-import WifiTetheringErrorIcon from '@mui/icons-material/WifiTetheringError';
+import WifiTetheringIcon from '@mui/icons-material/WifiTethering';
+import  { treeItemClasses } from '@mui/lab/TreeItem';
 import SwapCallsIcon from '@mui/icons-material/SwapCalls';
 import SettingsIcon from '@mui/icons-material/Settings';
-import { Box } from '@mui/material';
+import SubdirectoryArrowRightIcon from '@mui/icons-material/SubdirectoryArrowRight';
 
-const drawerWidth = 240;
+const Theme = createTheme({
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          height: "65px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-end",
+          backgroundColor: "white",
+        },
+      },
+    },
+    MuiAvatar: {
+      styleOverrides: {
+        root: {
+          margin: "30px",
+          cursor: "pointer",
+        },
+      },
+    },
+  },
+});
+const drawerWidth = 200;
 
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  [`& .${treeItemClasses.content}`]: {
+    color: theme.palette.text.secondary,
+    paddingRight: theme.spacing(1),
+    padding:theme.spacing(1),
+    fontWeight: theme.typography.fontWeightMedium,
+    '&.Mui-expanded': {
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+    '&:hover': {
+      backgroundColor: theme.palette.action.focusOpacity,
+    },
+    '&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused': {
+      backgroundColor: "#f0f4c3",
+      color: 'var(--tree-view-color)',
+      borderRight:"3px solid #7F961E"
+    },
+    [`& .${treeItemClasses.label}`]: {
+      fontWeight: 'inherit',
+      color: 'inherit',
+    },
+  },
+  [`& .${treeItemClasses.group}`]: {
+    marginLeft: 0,
+    [`& .${treeItemClasses.content}`]: {
+      paddingLeft: theme.spacing(2),
+    },
+  },
+}));
+
+function StyledTreeItem(props) {
+  const {
+    bgColor,
+    color,
+    labelIcon: LabelIcon,
+    labelInfo,
+    labelText,
+    ...other
+  } = props;
+
+  return (
+    <StyledTreeItemRoot
+      label={
+        <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
+          <Box component={LabelIcon} 
+          color="inherit"
+           sx={{ mr: 1 }} />
+          <Typography variant="body2" sx={{ fontWeight: 'inherit', flexGrow: 1 }}>
+            {labelText}
+          </Typography>
+          <Typography variant="caption" 
+          color="inherit"
+          >
+            {labelInfo}
+          </Typography>
+        </Box>
+      }
+      style={{
+        '--tree-view-color': color,
+        '--tree-view-bg-color': bgColor,
+      }}
+      {...other}
+    />
+  );
+}
+
+StyledTreeItem.propTypes = {
+  bgColor: PropTypes.string,
+  color: PropTypes.string,
+  labelIcon: PropTypes.elementType.isRequired,
+  labelInfo: PropTypes.string,
+  labelText: PropTypes.string.isRequired,
+};
 
 function AppDrawer(props) {
-  const menuList=[{'name':'Dashboard','icon':DeveloperBoardIcon}, {'name':'Recruitment','icon':ZoomInIcon}, {'name':'Onboarding','icon':AssessmentIcon},
-   {'name':'Requirement','icon':WifiTetheringErrorIcon},{'name':'Assignment','icon':SwapCallsIcon},{'name':'Settings','icon':SettingsIcon}];
-
-   function handleSelectedMenu(name){
-    console.log(name);
-   }
-   
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Permanent drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
+   
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <ThemeProvider theme={Theme}>
+        <AppBar
+          position="fixed"
+          sx={{
+            width: `calc(100% - ${drawerWidth}px)`,
+            ml: `${drawerWidth}px`,
+          }}
+        >
+          <Tooltip>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          </Tooltip>
+        </AppBar>
+        </ThemeProvider>
+        <Drawer
+          sx={{
             width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-        <List>
-          {menuList.map((text, index) => (
-            
-            <ListItem key={index} disablePadding >
-              <ListItemButton onClick={handleSelectedMenu(text)} >
-                <ListItemIcon>
-                  {<text.icon/>}
-                </ListItemIcon>
-                <ListItemText primary={text.name} />
-              </ListItemButton>
-            </ListItem>
-            
-          ))}
-        </List>
-        {/* <Divider /> */}
-        
-      </Drawer>
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open="true"
+        >
+          <DrawerHeader />
+          <Divider />
+          <TreeView
+      aria-label="gmail"
+      defaultCollapseIcon={<ArrowDropDownIcon />}
+      defaultExpandIcon={<ArrowRightIcon />}
+      defaultEndIcon={<div style={{ width: 24 }} />}
+      sx={{ height: 264, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+    >
+      <StyledTreeItem nodeId="1" labelText="Dashboard" labelIcon={ComputerOutlinedIcon} color="#7F961E"/>
+      <StyledTreeItem nodeId="2" labelText="Recruitment" labelIcon={ZoomInOutlinedIcon} color="#7F961E" />
+      <StyledTreeItem nodeId="3" labelText="Onboarding" labelIcon={AssessmentIcon} color="#7F961E"/>
+      <StyledTreeItem nodeId="4" labelText="Requirement" labelIcon={WifiTetheringIcon} color="#7F961E"/>
+      <StyledTreeItem nodeId="5" labelText="Assignment" labelIcon={SwapCallsIcon} color="#7F961E"/>
+      <StyledTreeItem nodeId="6" labelText="Settings" labelIcon={SettingsIcon} color="#7F961E">
+      <StyledTreeItem
+          nodeId="7"
+          labelText="Configuration"
+          labelIcon={SubdirectoryArrowRightIcon}
+          color="#7F961E"
+          bgColor="#f0f4c3"
+        />
+        <StyledTreeItem
+          nodeId="8"
+          labelText="Fellowship"
+          labelIcon={SubdirectoryArrowRightIcon}
+          color="#7F961E"
+          // bgColor="#fcefe3"
+        />
+        </StyledTreeItem>
+        </TreeView>
+        </Drawer>
       </Box>
-      );
+  );
 }
 
 export default AppDrawer;
+
+
+
